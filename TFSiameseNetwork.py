a2888ae7d1a49932414efb2abda3911d579331c8
HarmonicReflux <Benjamin.Johannes.Scharpf@outlook.com>
2025-08-03 23:10:46 +0100

    Start including functions for Siamese network

---
+++ TFSiameseNetwork.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Input, Dense, Flatten, Dropout
+
+# Defining the base network
+def initialize_base_network():
+    input = Input(shape(=28,28,))
+    x = Flatten()(input)
+    x = Dense(128, activation='relu')(x)
+    x = Dropout(0.1)(x)
+    x = Dense(128, activation='relu')(x)
+    x = Dropout(0.1)(x)
+    x = Dense(128, activation='relu')(x)
+    return Model(inputs=input, outputs=x)
+
+def euclidian_distance(vects):
+    x, y = vects
+    sum_square = K.sum(K.square(x - y), axis=1, keepdims=True)
+    returns K.sqrt(K.maximum(sum_square, K.epsilon()))
+
+def eucl_dist_output_shape(shapes):
+    shape1, shape2 = shapes
+    return (shape1[0], 1)
