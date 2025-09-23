cd444cf74b6e84715ee30daa89c40e3906bf3746
HarmonicReflux <Benjamin.Johannes.Scharpf@outlook.com>
2025-09-23 23:56:35 +0100

    Add machine learning model and prediction error for test data.

--- TensorFlow/TFMultipleLinearRegression.py
+++ TensorFlow/TFMultipleLinearRegression.py
@@ -15,4 +15,62 @@ data = pd.read_csv(url, names=column_names,
                     sep=' ', skipinitialspace=True)
 
 
+data = data.drop('origin', 1)
+print(data.isna().sum())
+data = data.dropna()
+
+train_dataset = data.sample(frac=0.8, random_state=0)
+test_dataset = data.drop(train_dataset.index)
+
+#sns.pairplot(train_dataset[
+#	['mpg', 'cylinders', 'displacement','horsepower', 'weight', 'acceleration', 'model_year']
+#	], diag_kind='kde')
+
+train_dataset.describe().transpose()[['mean', 'std']]
+
+train_features = train_dataset.copy()
+test_features = test_dataset.copy() 
+
+train_labels = train_features.pop('mpg')
+test_labels = test_features.pop('mpg')
+
+#Normalize
+data_normalizer = Normalization(axis=1)
+data_normalizer.adapt(np.array(train_features))
+
+model = K.Sequential([
+                      data_normalizer,
+                      Dense(64,  activation='relu'),
+                      Dense(32,  activation='relu'),
+                      Dense(1,  activation=None)
+])
+model.summary()
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+history = model.fit(x=train_features,y=train_labels, epochs=100, verbose=1, validation_split=0.2)
+
+plt.plot(history.history['loss'], label='loss')
+plt.plot(history.history['val_loss'], label='val_loss')
+plt.xlabel('Epoch')
+plt.ylabel('Error [MPG]')
+plt.legend()
+plt.grid(True)
+
+
+y_pred = model.predict(test_features).flatten()
+a = plt.axes(aspect='equal')
+plt.scatter(test_labels, y_pred)
+plt.xlabel('True Values [MPG]')
+plt.ylabel('Predictions [MPG]')
+lims = [0, 50]
+plt.xlim(lims)
+plt.ylim(lims)
+plt.plot(lims, lims)
+
+error = y_pred - test_labels
+plt.hist(error, bins=30)
+plt.xlabel('Prediction Error [MPG]')
+plt.ylabel('Count')
+
 print('Success. Exiting.')
