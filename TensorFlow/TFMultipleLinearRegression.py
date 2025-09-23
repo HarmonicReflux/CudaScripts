606bc591815e55bf708768e5e89ee4c9d75ab5f1
HarmonicReflux <Benjamin.Johannes.Scharpf@outlook.com>
2025-09-23 23:51:57 +0100

    Source publically available data and read it into pandas

--- TensorFlow/TFMultipleLinearRegression.py
+++ TensorFlow/TFMultipleLinearRegression.py
@@ -6,4 +6,13 @@ import tensorflow.keras as K
 from tensorflow.keras.layers import Dense, Normalization
 import seaborn as sns
 
-print('Succeeded. Exiting.')
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data' 
+column_names = ['mpg', 'cylinders', 'displacement', 'horsepower', 'weight',
+                'acceleration', 'model_year', 'origin']
+
+data = pd.read_csv(url, names=column_names,
+                    na_values='?', comment='\t',
+                    sep=' ', skipinitialspace=True)
+
+
+print('Success. Exiting.')
