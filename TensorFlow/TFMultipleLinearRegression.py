287057e6a237ab06c3097d707e6b401624187b1e
HarmonicReflux <Benjamin.Johannes.Scharpf@outlook.com>
2025-09-23 23:45:57 +0100

    Get started with multiple linear regression in TensorFlow

---
+++ TensorFlow/TFMultipleLinearRegression.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import tensorflow.keras as K
+from tensorflow.keras.layers import Dense, Normalization
+import seaborn as sns
+
+print('Succeeded. Exiting.')
